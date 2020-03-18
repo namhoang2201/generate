f078edf7df7910ea2c891e0d74bb5c369f534141
namhoang2201 <hoangnamtb95@gmail.com>
2020-03-18 12:42:31 +0700

    generate sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,4 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Simicart Generate</title>
-    <link rel="icon" type="image/png" href="./img/icon-logo.png">
-    <link rel="stylesheet" type="text/css" href="./css/bootstrap/bootstrap.min.css"/>
-    <link rel="stylesheet" type="text/css" href="./css/bootstrap/bootstrap.min.css.map"/>
-    <link rel="stylesheet" type="text/css" href="./css/index.css"/>
-</head>
-<body>
-<?php
-$swcontent = "
+
         'use strict';
         self.addEventListener(
             'install', function (event) {
@@ -164,45 +149,4 @@ $swcontent = "
                 );
             }
         );
-        ";
-$fileToSave = $_SERVER['DOCUMENT_ROOT'].'/generate/sw.js';
-if (!file_exists($fileToSave)) {
-    chmod($_SERVER['DOCUMENT_ROOT'].'/generate/', 0777);
-    file_put_contents($fileToSave, $swcontent);
-    chmod($fileToSave, 0777);
-}
-?>
-<div id="root">
-
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 col-xl-12 col-xs-12">
-                <div class="wrap">
-                    <div class="logo">
-                        <a href="./index.php" target="_self"><img class="img-responsive"
-                                                                  src="./img/icon-logo.png"
-                                                                  alt="simicart-logo"></a>
-                    </div>
-                    <div class="title">
-                        Generate manifest and service worker.
-                    </div>
-                </div>
-                <div class="selection">
-                    <div class="manifest">
-                        <a href="./manifest.php">Generate manifest.json</a>
-                    </div>
-                    <div class="service-worker">
-                        <a href="./downoad.php" target="_self">Generate sw.js</a>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </div>
-</div>
-
-<script defer type="text/javascript" src="./js/jquery.min.js"></script>
-<script defer type="text/javascript" src="./js/bootstrap.min.js"></script>
-<script defer type="text/javascript" src="./js/index.js"></script>
-</body>
-</html>
+        
